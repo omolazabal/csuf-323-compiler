079620876d445928f9d891477753d3609dd3c581
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-06 02:24:54 -0700

    initial commit

---
+++ rat19f/lexer.py
@@ -0,0 +1,91 @@
+
+class Lexer:
+    state    = None   # State table
+    colstr   = None   # Determine state table column
+    keywords = None   # List of keywords
+
+    def __init__(self):
+        self.state = [
+        #    0  1  2  3  4 
+            [1, 2, 5, 5, 5], # 0
+            [1, 1, 1, 5, 5], # 1  Identifier
+            [5, 2, 5, 3, 5], # 2  Integer
+            [5, 4, 5, 5, 5], # 3
+            [5, 4, 5, 5, 5], # 4  Real
+            [5, 5, 5, 5, 5], # 5  
+        ]
+        self.colstr = [
+            'abcdefghijklmnopqrstuvwxyz',
+            '0123456789',
+            '_',
+            '.'
+        ]
+        self.keywords = [
+            'function', 'int', 'boolean', 'real', 'if', 'fi', 'otherwise',
+            'return', 'put', 'get', 'while', 'true', 'false'
+        ]
+        self.operators = [ '=', '>', '<', '+', '-', '*', '/']
+        self.seperators = ['(', ')', '{', '}', ',', ';']
+        self.combination_operators = ['==', '\=', '=>', '<=']
+        self.combination_seperators = ['%%', '[*', '*]']
+
+    def position(self, char):
+        for i, col in enumerate(self.colstr):
+            if char.lower() in col: return i
+        return len(self.state[0]) - 1
+
+    def run(self, text):
+        symbol_table = []
+        lex_state = 0  # Row of state table
+        currentp = 0  # Pointer to current character
+        startp = 0  # Token start pointer
+        token_class = ''
+        while currentp < len(text):
+            currentc = text[currentp]  # Current character
+            col = self.position(currentc)  # Column of state table
+            lex_state = self.state[lex_state][col]
+            if lex_state == 1:  # Potential identifier
+                token = text[startp:currentp+1]
+                token_class = 'keyword' if token in self.keywords else 'identifier'
+            elif lex_state == 2:  # Potential integer
+                token = text[startp:currentp+1]
+                token_class = 'integer'
+            elif lex_state == 4:  # Potential real
+                token = text[startp:currentp+1]
+                token_class = 'real'
+            elif lex_state == 5:
+                selection = \
+                    'operator' if currentc in self.operators else \
+                    'seperator' if currentc in self.seperators else \
+                    ''
+                # Combination operators
+                if selection is 'operator' and (symbol_table[-1][1] + currentc) in self.combination_operators:
+                    symbol_table[-1][1] = symbol_table[-1][1] + currentc
+                # Single operators
+                elif selection:
+                    if token_class:
+                        symbol_table.append([token_class, token])
+                    token = currentc
+                    token_class = selection
+                    symbol_table.append([token_class, token])
+                elif token_class:
+                    symbol_table.append([token_class, token])
+                token_class = ''
+                lex_state = 0
+            else:
+                token_class = ''
+            currentp += 1
+            if lex_state == 0:
+                startp = currentp
+
+        if token_class:
+            symbol_table.append((token_class, token))
+
+        for row in symbol_table:
+            print(row)
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+
+from rat19f import Lexer
+
+a = Lexer()
+with open('input.rat', 'r') as f:
+    content = f.read()
+a.run(content)
