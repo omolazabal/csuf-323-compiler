a94fd5806044fc983c20ac62b3ff1fafbb048abb
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-11-10 23:43:32 -0800

    memory efficient lexer

--- rat19f/lexer.py
+++ rat19f/lexer.py
@@ -1,15 +1,27 @@
 
+class Set:
+    def __init__(self, comment=False):
+        self.comment = comment
+
+    def values(self, *args):
+        if not self.comment:
+            return tuple(args)
+        else:
+            return None
+
+
 class Lexer:
     state                  = None   # State table
     colstr                 = None   # Determine state table column
     keywords               = None   # List of keywords
     operators              = None   # List of valid operators
-    seperators             = None   # List of valid seperators
+    separators             = None   # List of valid separators
     combination_operators  = None   # List of valid combined operators
-    combination_seperators = None   # List of valid combined seperators
+    combination_separators = None   # List of valid combined separators
     whitespaces            = None   # Types of whitespaces
 
-    def __init__(self):
+    def __init__(self, filename):
+        self.filename = filename
         self.state = [
         #    0  1  2  3  4 
             [1, 2, 5, 5, 5], # 0
@@ -31,9 +43,9 @@ class Lexer:
             'return', 'put', 'get', 'while', 'true', 'false'
         ]
         self.operators = [ '=', '>', '<', '+', '-', '*', '/']
-        self.seperators = ['(', ')', '{', '}', ',', ';']
+        self.separators = ['(', ')', '{', '}', ',', ';']
         self.combination_operators = ['==', '/=', '=>', '<=']
-        self.combination_seperators = ['%%', '[*', '*]']
+        self.combination_separators = ['%%', '[*', '*]']
         self.whitespaces = [' ', '\t', '\n']
 
     def position(self, char):
@@ -42,80 +54,100 @@ class Lexer:
             if char.lower() in col: return i
         return len(self.state[0]) - 1
 
-    def scan(self, text):
+    def is_in_combination(self, char, first=False, second=False):
+        if first and second:
+            return (char in [op[0] for op in self.combination_operators] and char in [op[1] for op in self.combination_operators]) or \
+                   (char in [sp[0] for sp in self.combination_separators] and char in [sp[1] for sp in self.combination_separators])
+        elif first:
+            return (char in [op[0] for op in self.combination_operators]) or (char in [sp[0] for sp in self.combination_separators])
+        elif second:
+            return (char in [op[1] for op in self.combination_operators]) or (char in [sp[1] for sp in self.combination_separators])
+        return False
+
+
+    def scanner(self):
         '''Performs parsing, leveraging the state table'''
-        lexemes = []           # Table entries for lexemes
-        comments = []          # Table entries for comments, in case they serve a use for future
-        symbol_table = lexemes # The active table
+        prev_op_sep = False
         lex_state = 0          # Row of state table
         currentp = 0           # Pointer to current character
-        startp = 0             # Token start pointer
+        token = ''
         token_class = ''
-        while currentp < len(text):
-            # Scan text until EOF
-            currentc = text[currentp]      # Current character
-            col = self.position(currentc)  # Column of state table
-            lex_state = self.state[lex_state][col]
-            if lex_state == 1:  # Potential identifier
-                token = text[startp:currentp+1]
-                token_class = 'keyword' if token in self.keywords else 'identifier'
-            elif lex_state == 2:  # Potential integer
-                token = text[startp:currentp+1]
-                token_class = 'integer'
-            elif lex_state == 4:  # Potential real
-                token = text[startp:currentp+1]
-                token_class = 'real'
-            elif lex_state == 5:
-                selection = \
-                    'operator' if currentc in self.operators else \
-                    'seperator' if currentc in self.seperators else \
-                    ''
-                # Combination seperators
-                if len(symbol_table) and (symbol_table[-1][1] + currentc) in self.combination_seperators:
-                    token = symbol_table[-1][1] + currentc
-                    if token == '[*':
-                        symbol_table.pop()
-                        symbol_table = comments  # Swap to comments table if ongoing comment
-                    elif token == '*]':
-                        symbol_table[-1] = ['seperator', token]
-                        symbol_table = lexemes  # Swap to lexemes table if comments stop
+        current_token = []
+        set_ = Set()
+
+        with open(self.filename) as fileobj:
+            for line in fileobj:
+                for currentc in line:
+                    col = self.position(currentc)
+                    lex_state = self.state[lex_state][col]
+                    current_token.append(currentc)
+
+                    if not self.is_in_combination(currentc, second=True) and prev_op_sep:
+                        # A single operator has been stored, and the combination instance does not exist
+                        prev_op_sep = False
+                        yield set_.values(selection, token)
+
+                    # Check the States
+                    if lex_state == 1:
+                        # Potential identifier
+                        token = ''.join(current_token)
+                        token_class = 'keyword' if token in self.keywords else 'identifier'
+                    elif lex_state == 2:
+                        # Potential integer
+                        token = ''.join(current_token)
+                        token_class = 'integer'
+                    elif lex_state == 4:
+                        # Potential real
+                        token = ''.join(current_token)
+                        token_class = 'real'
+                    elif lex_state == 5:
+                        # Other cases (operator, separator, unknowns, comments, etc)
+                        selection = \
+                            'operator' if currentc in self.operators else \
+                            'separator' if currentc in self.separators else \
+                            ''
+                        if token and (token + currentc) in self.combination_separators:
+                            # Combination separators
+                            prev_op_sep = False
+                            token = token + currentc
+                            if token == '[*':
+                                # Ignore output
+                                set_.comment = True
+                            elif token == '*]':
+                                yield set_.values('separator', token)
+                                set_.comment =  False
+                            else:
+                                yield set_.values('separator', token)
+                        elif token and selection is 'operator' and (token + currentc) in self.combination_operators:
+                            # Combination operators
+                            prev_op_sep = False
+                            token = token + currentc
+                            yield set_.values('operator', token)
+                        elif selection:
+                            # Single operators & separators
+                            if token_class:
+                                yield set_.values(token_class, token)
+                            token = currentc
+                            token_class = selection
+                            prev_op_sep = True
+                        # Empty state
+                        elif token_class:
+                            yield set_.values(token_class, token)
+                        # Unknown symbols
+                        elif currentc not in self.whitespaces:
+                            token = currentc
+                            token_class = 'unknown'
+                            if not self.is_in_combination(token, first=True): 
+                                yield set_.values(token_class, token)
+                        # Reset the state if no potential ending state
+                        token_class = ''
+                        lex_state = 0
                     else:
-                        symbol_table[-1] = ['seperator', token]
-                # Combination operators
-                elif selection is 'operator' and (symbol_table[-1][1] + currentc) in self.combination_operators:
-                    symbol_table[-1][1] = symbol_table[-1][1] + currentc
-                # Single operators
-                elif selection:
-                    if token_class:
-                        symbol_table.append([token_class, token])
-                    token = currentc
-                    token_class = selection
-                    symbol_table.append([token_class, token])
-                # Empty state
-                elif token_class:
-                    symbol_table.append([token_class, token])
-                # Unknown symbols
-                elif currentc not in self.whitespaces:
-                    token = currentc
-                    token_class = 'unknown'
-                    symbol_table.append([token_class, token])
-                # Reset the state if no potential ending state
-                token_class = ''
-                lex_state = 0
-            else:
-                token_class = ''
-            currentp += 1
-            if lex_state == 0:
-                startp = currentp
+                        token_class = ''
+                    if lex_state == 0:
+                        current_token.clear()
 
         if token_class:
             # Grab last remaining token
-            symbol_table.append((token_class, token))
-        symbol_table = lexemes
-        return symbol_table
-
-
-
-
-
+            yield set_.values(token_class, token)
 
